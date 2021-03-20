99fb15cea24320a5d0cd0f596840eaddb9b8df6f
Madison Swain-Bowden <bowdenm@spu.edu>
2021-03-20 16:57:26 -0700

    WIP Portland DB

---
+++ api/data/portland_db.go
@@ -0,0 +1,356 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/gobuffalo/nulls"
+	"github.com/jackc/pgx/v4"
+)
+
+// PortlandOfficer is the object model for PPB officers
+type PortlandOfficer struct {
+	FirstName                   nulls.String    `json:"first_name,omitempty"`
+	LastName                    nulls.String    `json:"last_name,omitempty"`
+	Gender                      nulls.String    `json:"gender,omitempty"`
+	OfficerRank                 nulls.String    `json:"officer_rank,omitempty"`
+	EmployeeID                  nulls.String    `json:"employee_id,omitempty"`
+	HelmetID                    nulls.String    `json:"helmet_id,omitempty"`
+	HelmetIDThreeDigits         nulls.String    `json:"helmet_id_three_digits,omitempty"`
+	Salary                      nulls.String    `json:"salary,omitempty"`
+	Badge                       nulls.String    `json:"badge,omitempty"`
+	CopsPhotoProfileLink        nulls.String    `json:"cops_photo_profile_link,omitempty"`
+	CopsPhotoHasPhoto           nulls.String    `json:"cops_photo_has_photo,omitempty"`
+	Employed_3_12_21            nulls.String    `json:"employed_3_12_21,omitempty"`
+	Employed_12_28_20           nulls.String    `json:"employed_12_28_20,omitempty"`
+	Employed_10_01_20           nulls.String    `json:"employed_10_01_20,omitempty"`
+	Retired_6_1_20              nulls.String    `json:"retired_6_1_20,omitempty"`
+	RetiredOrCertRevoked        nulls.String    `json:"retired_or_cert_revoked,omitempty"`
+	RetiredOrCertRevokedDate    nulls.String    `json:"retired_or_cert_revoked_date,omitempty"`
+	HireYear                    nulls.String    `json:"hire_year,omitempty"`
+	HireDate                    nulls.String    `json:"hire_date,omitempty"`
+	StateCertDate               nulls.String    `json:"state_cert_date,omitempty"`
+	StateCertLevel              nulls.String    `json:"state_cert_level,omitempty"`
+	RRT                         nulls.String    `json:"rrt,omitempty"`
+	RRT2016                     nulls.String    `json:"rrt_2016,omitempty"`
+	RRT2018NiiyaEmail           nulls.String    `json:"rrt_2018_niiya_email,omitempty"`
+	RRT2018                     nulls.String    `json:"rrt_2018,omitempty"`
+	RRT2019                     nulls.String    `json:"rrt_2019,omitempty"`
+	RRT2020                     nulls.String    `json:"rrt_2020,omitempty"`
+	SoundTruckTraining          nulls.String    `json:"sound_truck_training_2020,omitempty"`
+	InstructedForDpsst          nulls.String    `json:"instructed_for_dpsst,omitempty"`
+	InstructedForLessLethal     nulls.String    `json:"instructed_for_less_lethal,omitempty"`
+	InvolvedInOisUof            nulls.String    `json:"involved_in_ois_uof,omitempty"`
+	Notes                       nulls.String    `json:"notes,omitempty"`
+}
+
+// PortlandOfficerMetadata retrieves metadata describing the PortlandOfficer struct
+func (c *Client) PortlandOfficerMetadata() *DepartmentMetadata {
+	return &DepartmentMetadata{
+		Fields: []map[string]string{
+            {
+                "FieldName": "first_name",
+                "Label": "First Name",
+            },
+            {
+                "FieldName": "last_name",
+                "Label": "Last Name",
+            },
+            {
+                "FieldName": "gender",
+                "Label": "Gender",
+            },
+            {
+                "FieldName": "officer_rank",
+                "Label": "Rank",
+            },
+            {
+                "FieldName": "employee_id",
+                "Label": "Employee (Chest) ID",
+            },
+            {
+                "FieldName": "helmet_id",
+                "Label": "Helmet #",
+            },
+            {
+                "FieldName": "helmet_id_three_digit",
+                "Label": "3-Digit Helmet #",
+            },
+            {
+                "FieldName": "salary",
+                "Label": "Fiscal Earnings 2019",
+            },
+            {
+                "FieldName": "badge",
+                "Label": "Badge/DPSST Number",
+            },
+            {
+                "FieldName": "cops_photo_profile_link",
+                "Label": "Cops.Photo Profile Link",
+            },
+            {
+                "FieldName": "cops_photo_has_photo",
+                "Label": "Pic on Cops.photo (y/n)",
+            },
+            {
+                "FieldName": "employed_3_12_21",
+                "Label": "Employed as of 3/12/21",
+            },
+            {
+                "FieldName": "employed_12_28_20",
+                "Label": "Employed as of 12/28/20",
+            },
+            {
+                "FieldName": "employed_10_01_20",
+                "Label": "Employed as of 10/01/20",
+            },
+            {
+                "FieldName": "retired_6_1_20",
+                "Label": "Retired/Resigned as of 6/1/20",
+            },
+            {
+                "FieldName": "retired_or_cert_revoked",
+                "Label": "Retired/Resigned as of 6/1/20 OR Cert Revoked (ever)",
+            },
+            {
+                "FieldName": "retired_or_cert_revoked_date",
+                "Label": "Date of Cert Revoke",
+            },
+            {
+                "FieldName": "hire_year",
+                "Label": "Hire Year",
+            },
+            {
+                "FieldName": "hire_date",
+                "Label": "Hire Date",
+            },
+            {
+                "FieldName": "state_cert_date",
+                "Label": "State Certification Date",
+            },
+            {
+                "FieldName": "state_cert_level",
+                "Label": "State Certification Level",
+            },
+            {
+                "FieldName": "rrt",
+                "Label": "RRT (Rapid Response Team) Member",
+            },
+            {
+                "FieldName": "rrt_2016",
+                "Label": "RRT member as of 2016 via 2017 PPB AR",
+            },
+            {
+                "FieldName": "rrt_2018_niiya_email",
+                "Label": "RRT member as of 2018 via Niiya Email",
+            },
+            {
+                "FieldName": "rrt_2018",
+                "Label": "RRT Specific Training 2018",
+            },
+            {
+                "FieldName": "rrt_2019",
+                "Label": "RRT Specific Training 2019",
+            },
+            {
+                "FieldName": "rrt_2020",
+                "Label": "RRT Specific Training 2020",
+            },
+            {
+                "FieldName": "sound_truck_training_2020",
+                "Label": "Sound Truck Training 2020",
+            },
+            {
+                "FieldName": "instructed_for_dpsst",
+                "Label": "Has Instructed Course for DPSST 2017+",
+            },
+            {
+                "FieldName": "instructed_for_less_lethal",
+                "Label": "Instructor for Less Lethal/Chemical Weapons Courses",
+            },
+            {
+                "FieldName": "involved_in_ois_uof",
+                "Label": "Has Been Involved in OIS/Significant UoF Incident",
+            },
+            {
+                "FieldName": "notes",
+                "Label": "Notes",
+            }
+		},
+		LastAvailableRosterDate: "2021-03-12",
+		Name:                    "Portland PD",
+		ID:                      "ppb",
+		SearchRoutes: map[string]*SearchRouteMetadata{
+			"exact": {
+				Path:        "/seattle/officer",
+				QueryParams: []string{"badge", "first_name", "last_name"},
+			},
+			"fuzzy": {
+				Path:        "/seattle/officer/search",
+				QueryParams: []string{"first_name", "last_name"},
+			},
+		},
+	}
+}
+
+// SeattleGetOfficerByBadge invokes seattle_get_officer_by_badge_p
+func (c *Client) SeattleGetOfficerByBadge(badge string) (*SeattleOfficer, error) {
+	ofc := SeattleOfficer{}
+	err := c.pool.QueryRow(context.Background(),
+		`
+			SELECT
+				date,
+				badge,
+				full_name,
+				first_name,
+				middle_name,
+				last_name,
+				title,
+				unit,
+				unit_description
+			FROM seattle_get_officer_by_badge_p (badge := $1);
+		`,
+		badge,
+	).Scan(
+		&ofc.Date,
+		&ofc.Badge,
+		&ofc.FullName,
+		&ofc.FirstName,
+		&ofc.MiddleName,
+		&ofc.LastName,
+		&ofc.Title,
+		&ofc.Unit,
+		&ofc.UnitDescription,
+	)
+
+	return &ofc, err
+}
+
+// SeattleSearchOfficerByName invokes seattle_search_officer_by_name_p
+func (c *Client) SeattleSearchOfficerByName(firstName, lastName string) ([]*SeattleOfficer, error) {
+	rows, err := c.pool.Query(context.Background(),
+		`
+			SELECT
+				date,
+				badge,
+				first_name,
+				middle_name,
+				last_name,
+				title,
+				unit,
+				unit_description
+			FROM seattle_search_officer_by_name_p(first_name := $1, last_name := $2);
+		`,
+		firstName,
+		lastName,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return seattleMarshalOfficerRows(rows)
+}
+
+// SeattleFuzzySearchByName invokes seattle_fuzzy_search_officer_by_name_p
+func (c *Client) SeattleFuzzySearchByName(name string) ([]*SeattleOfficer, error) {
+	rows, err := c.pool.Query(context.Background(),
+		`
+			SELECT
+				date,
+				badge,
+				full_name,
+				first_name,
+				middle_name,
+				last_name,
+				title,
+				unit,
+				unit_description
+			FROM seattle_fuzzy_search_officer_by_name_p(full_name_v := $1);
+		`,
+		name,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return seattleMarshalOfficerRows(rows)
+}
+
+// SeattleFuzzySearchByFirstName invokes seattle_fuzzy_search_officer_by_first_name_p
+func (c *Client) SeattleFuzzySearchByFirstName(firstName string) ([]*SeattleOfficer, error) {
+	rows, err := c.pool.Query(context.Background(),
+		`
+			SELECT
+				date,
+				badge,
+				full_name,
+				first_name,
+				middle_name,
+				last_name,
+				title,
+				unit,
+				unit_description
+			FROM seattle_fuzzy_search_officer_by_first_name_p(first_name := $1);
+		`,
+		firstName,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return seattleMarshalOfficerRows(rows)
+}
+
+// SeattleFuzzySearchByLastName invokes seattle_fuzzy_search_officer_by_last_name_p
+func (c *Client) SeattleFuzzySearchByLastName(lastName string) ([]*SeattleOfficer, error) {
+	rows, err := c.pool.Query(context.Background(),
+		`
+			SELECT
+				date,
+				badge,
+				full_name,
+				first_name,
+				middle_name,
+				last_name,
+				title,
+				unit,
+				unit_description
+			FROM seattle_fuzzy_search_officer_by_last_name_p(last_name := $1);
+		`,
+		lastName,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return seattleMarshalOfficerRows(rows)
+}
+
+func seattleMarshalOfficerRows(rows pgx.Rows) ([]*SeattleOfficer, error) {
+	officers := []*SeattleOfficer{}
+	for rows.Next() {
+		ofc := SeattleOfficer{}
+		err := rows.Scan(
+			&ofc.Date,
+			&ofc.Badge,
+			&ofc.FullName,
+			&ofc.FirstName,
+			&ofc.MiddleName,
+			&ofc.LastName,
+			&ofc.Title,
+			&ofc.Unit,
+			&ofc.UnitDescription,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		officers = append(officers, &ofc)
+	}
+	return officers, nil
+}
