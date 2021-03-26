a5f7db31410a43dbec82e2316670b109e3eecbd2
Marcus Puckett <mpuckett259@gmail.com>
2021-03-25 17:48:10 -0700

    Fuck it we're going with shell scripts
    Because Docker on Linux is not the same as Docker on anything else I was running into a lot of issues with file permissions, specifically around the desire to expose the Postgres server's log files back out to the host to allow for some nice fail2ban shenanigans. After some pain (see commit history) I decided that it would just be easier to just use the ./init-script.sh to spin up the docker containers, then run a quick chown with the in container postgres uid:gid, which is 70:70 and appears to be static? Hopefully? Anyway it works so what ever.
    
    I also stopped using docker volumes and am just copying the necessary config files in via the dockerfile now as God intended.

--- init-script.sh
+++ init-script.sh
@@ -1,33 +1,43 @@
 #!/bin/sh
 
-while getopts udrD flag
+while getopts udrDf: flag
 do
   case "${flag}" in
     u) up="true";;
     d) down="true";;
     r) restart="true";;
     D) detached="-d";;
+    f) file="$OPTARG";;
     *) error="true";;
   esac
 done
 
+if [ -z "$file" ]
+then
+  file = "docker-compose.yml"
+fi
+
 if [ "$up" = "true" ]
 then
   docker volume rm spd-lookup_db_data
-  docker-compose up --build $detached
+  docker-compose -f $file up --build $detached
+  chown -R 70:70 db/db_logs
 fi
 
 if [ "$down" = "true" ]
 then
-  docker-compose down
+  docker-compose -f $file down
   docker volume rm spd-lookup_db_data
+  rm -rf db/db_logs
 fi
 
 if [ "$restart" = "true" ]
 then
-  docker-compose down
+  docker-compose -f $file down
   docker volume rm spd-lookup_db_data
-  docker-compose up --build $detached
+  rm -rf db/db_logs
+  docker-compose -f $file up --build $detached
+  chown -R 70:70 db/db_logs
 fi
 
 if [ "$error" = "true" ]
