6de094406ce70fa472d8819b089b3e7af1a2bf5f
Marcus Puckett <mpuckett259@gmail.com>
2021-06-15 19:46:27 -0700

    Fix python data prep script
    The script wasn't working how I remembered it working so I fixed it so it did.

--- scripts/prep-spd-roster.py
+++ scripts/prep-spd-roster.py
@@ -23,9 +23,9 @@ def main():
     df.rename(columns=column_names, inplace=True)
 
     # Split the names out
-    df.apply(lambda x: first_name(x), axis=1)
-    df.apply(lambda x: middle_name(x), axis=1)
-    df.apply(lambda x: last_name(x), axis=1)
+    df['first_name'] = df.apply(lambda x: first_name(x), axis=1)
+    df['middle_name'] = df.apply(lambda x: middle_name(x), axis=1)
+    df['last_name'] = df.apply(lambda x: last_name(x), axis=1)
 
     # Add date column to DataFrame
     df['date'] = date
@@ -35,17 +35,19 @@ def main():
 
 
 def first_name(df: DataFrame):
-    df['first_name'] = df['full_name'].split(' ')[1].strip()
+    return df['full_name'].split(' ')[1].strip()
 
 
 def middle_name(df: DataFrame):
     name_list = df['full_name'].split(' ')
     if len(name_list) > 2:
-        df['middle_name'] = ' '.join(name_list[2:])
+        return ' '.join(name_list[2:])
+    else:
+        return ''
 
 
 def last_name(df: DataFrame):
-    df['last_name'] = df['full_name'].split(' ')[0].strip(' ,')
+    return df['full_name'].split(' ')[0].strip(' ,')
 
 
 if __name__ == "__main__":
