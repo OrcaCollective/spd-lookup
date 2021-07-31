e62b0c77b0a038a63ae74397d5dd96205bbe7fdc
Marcus Puckett <mpuckett259@gmail.com>
2021-07-31 15:33:37 -0700

    Add new sql files
    Add associated SQL set up files for the new police orgs:
     * Auburn
     * Lakewood
     * Olympia
    
    Needed to update the numbers on the sql files so they get executed in the right order so had to update the read only user creation and remote access shell script file names as well.

---
+++ db/sql/7_olympia.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS olympia_officers (
+    id                  SERIAL PRIMARY KEY,
+    date                DATE,
+    first_name		    VARCHAR(100),
+    last_name           VARCHAR(100),
+    title               VARCHAR(100),
+    unit                VARCHAR(50),
+    badge               VARCHAR(10)
+);
+
+COPY olympia_officers (date,first_name,last_name,title,unit,badge)
+FROM '/seed/Olympia-WA-Police-Department_05-01-21.csv' DELIMITER ',' CSV HEADER;
